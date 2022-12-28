4328cd13af8a30fbaeb45c845311355dfd75f498
Prince Narteh <prinarteh@gmail.com>
2022-12-28 02:58:13 +0000

    add button to add note to sidebar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -8,6 +8,8 @@ import { GiNotebook } from "react-icons/gi";
 import { MdArrowBackIosNew, MdOutlineFavoriteBorder } from "react-icons/md";
 import { TbLogout } from "react-icons/tb";
 import { signOut } from "next-auth/react";
+import { IoMdAdd } from "react-icons/io";
+import Link from "next/link";
 
 const links = [
   {
@@ -45,7 +47,7 @@ const SideBar = ({
 }) => {
   return (
     <div
-      className={`absolute h-[calc(100vh_-_40px)] w-72 bg-gray-900 text-white rounded-l-2xl p-5 duration-300 ${
+      className={`absolute h-[calc(100vh_-_40px)] bg-gray-900 text-white rounded-l-2xl p-5 duration-300 ${
         open ? "w-72" : "w-16"
       }`}
     >
@@ -58,36 +60,54 @@ const SideBar = ({
         />
       </div>
       <div className="flex flex-col justify-between h-full pb-5">
-        <div className={`py-5 duration-300`}>
-          <h3
-            className={`font-bold whitespace-pre duration-300 ${
-              !open && "-translate-x-10 opacity-0 invisible"
-            }`}
+        <div>
+          <Link
+            href={"/notes/add"}
+            className="bg-blue-500 w-full mt-5 py-1 rounded-full flex items-center justify-center"
           >
-            Quick links
-          </h3>
-          <ul className={`space-y-1`}>
-            {links.map((link, index) => (
-              <li
-                key={index}
-                className={`py-1.5 rounded-md flex items-center gap-3.5 cursor-pointer ${
-                  open && "pl-3 hover:bg-blue-500"
-                } duration-300`}
-              >
-                <div className={`${open ? "text-lg" : "text-xl"} duration-300`}>
-                  {React.createElement(link.icons)}
-                </div>
-                <h3
-                  style={{ transitionDelay: `${open && index + 1 + "00ms"}` }}
-                  className={`whitespace-pre ${
-                    !open && "translate-x-10 opacity-0 invisible"
+            <IoMdAdd className="inline-block" />
+            <span
+              className={`whitespace-pre ${
+                !open && "translate-x-10 opacity-0 invisible"
+              } duration-300`}
+            >
+              Add Note
+            </span>
+          </Link>
+
+          <div className={`py-5 duration-300`}>
+            <h3
+              className={`font-bold whitespace-pre duration-300 ${
+                !open && "-translate-x-10 opacity-0 invisible"
+              }`}
+            >
+              Quick links
+            </h3>
+            <ul className={`space-y-1`}>
+              {links.map((link, index) => (
+                <li
+                  key={index}
+                  className={`py-1.5 rounded-md flex items-center gap-3.5 cursor-pointer ${
+                    open && "pl-3 hover:bg-blue-500"
                   } duration-300`}
                 >
-                  {link.label}
-                </h3>
-              </li>
-            ))}
-          </ul>
+                  <div
+                    className={`${open ? "text-lg" : "text-xl"} duration-300`}
+                  >
+                    {React.createElement(link.icons)}
+                  </div>
+                  <h3
+                    style={{ transitionDelay: `${open && index + 1 + "00ms"}` }}
+                    className={`whitespace-pre ${
+                      !open && "translate-x-10 opacity-0 invisible"
+                    } duration-300`}
+                  >
+                    {link.label}
+                  </h3>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
         <div
           className={`py-1.5 rounded-md flex items-center gap-3.5 cursor-pointer ${
